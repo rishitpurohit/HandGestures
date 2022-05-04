a641b78a315d7aaa46ee251e05340abe2a797898
rishitpurohit <94513622+rishitpurohit@users.noreply.github.com>
2022-05-04 15:58:22 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,37 +1,79 @@
-//https://teachablemachine.withgoogle.com/models/9iZXrlziM/
+
+
+//https://teachablemachine.withgoogle.com/models/RIlIgylLG/
 Webcam.set({
-    width : 350 , 
+    width:350,
     height:300,
-    image_format:"png",
-    png_quality:95
-    
-});
+    image_format : 'png',
+    png_quality:90
+  });
 
-camera=document.getElementById("camera");
+  camera = document.getElementById("camera");
 
-Webcam.attach('camera');
+Webcam.attach( '#camera' );
 
-
-
-function take_snapshot(){
-    Webcam.snap(function(data_uri){
-    document.getElementById("result").innerHTML="<img id='selfie_image' src='"+data_uri+"'/>"; 
+      
+function take_snapshot()
+{
+    Webcam.snap(function(data_uri) {
+        document.getElementById("result").innerHTML = '<img id="captured_image" src="'+data_uri+'"/>';
     });
 }
 
-console.log("ml5 version"+ml5.version);
+  console.log('ml5 version:', ml5.version);
+  
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/RIlIgylLG/model.json',modelLoaded);
 
-classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/9iZXrlziM/model.json',modelLoaded);
+  function modelLoaded() {
+    console.log('Model Loaded!');
+  }
+      
+  function check()
+  {
+    img = document.getElementById('captured_image');
+    classifier.classify(img, gotResult);
+  }
 
-function modelLoaded(){
-    console.log("model loaded")
+function gotResult(error, results) {
+  if (error) {
+    console.error(error);
+  } else {
+    console.log(results);
+    
+    document.getElementById("result_object_name").innerHTML = results[0].label;
+
+    gesture = results[0].label;
+    
+    toSpeak = "";
+    
+    if(gesture == "amazing")
+    {
+      toSpeak = "This is looking amazing";
+      document.getElementById("result_object_gesture_icon").innerHTML = "&#128076;";
+    }
+    else if(gesture == "best")
+    {
+      toSpeak = "All the best";
+      document.getElementById("result_object_gesture_icon").innerHTML = "&#128077;";
+    }
+    else if(gesture == "victory")
+    {
+      toSpeak = "That was the marvelous victory";
+      document.getElementById("result_object_gesture_icon").innerHTML = "&#9996;";
+    }
+
+    speak();
+  }
 }
+
 
 function speak(){
     var synth = window.speechSynthesis;
-  var  speak_data_1 = "the first prediction is "+ predicition_1;
-  var  speak_data_2 = "the second prediction is "+ predicition_2;
-  var utterThis= new SpeechSynthesisUtterance(speak_data_1+speak_data_1);
-  synth.speak(utterThis);
-}
 
+    speak_data = toSpeak;
+
+    var utterThis = new SpeechSynthesisUtterance(speak_data);
+
+    synth.speak(utterThis);
+
+}
